f923dff20deca78a145a6cbcb8a0ee3aa43fc228
MiguelManente <manente2011@gmail.com>
2023-11-22 16:13:45 -0300

    Se modificó el archivo borrar elementos

--- Vectores/BorrarEleVector.cpp
+++ Vectores/BorrarEleVector.cpp
@@ -7,16 +7,16 @@ int main ()
 {
   vector <int> lista;
 
-  // set some values (from 1 to 10)
+  // agrega al vector números del 1 al 10
   for (int i=1; i<=10; i++){
       lista.push_back(i);
   } 
 
- /*  // erase the 6th element
-  lista.erase (lista.begin()+5); */
+ // borra los tres primeros elementos de la lista 
+  //lista.erase (lista.begin(),lista.begin()+3);
 
-  // erase the first 3 elements:
-  lista.erase (lista.begin(),lista.begin()+5);
+  // Borra los siete primeros elementos de la lista:
+  lista.erase (lista.begin(),lista.end()-3);
 
   cout << "El vector queda asi:";
   for (unsigned i=0; i< lista.size(); ++i)
