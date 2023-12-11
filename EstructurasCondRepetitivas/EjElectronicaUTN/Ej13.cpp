154763dc14e45927cf55739dc1a6192eee2b2e84
MiguelManente <manente2011@gmail.com>
2023-12-11 15:43:01 -0300

    Ejercicio calculo múltiplo de un número

--- EstructurasCondRepetitivas/EjElectronicaUTN/Ej13.cpp
+++ EstructurasCondRepetitivas/EjElectronicaUTN/Ej13.cpp
@@ -1,1 +1,31 @@
-13-Generar e informar los primeros N múltiplos de un número M entero
+/* EJERCICIO 13:
+   Generar e informar los primeros N múltiplos de un número M entero*/
+
+#include<iostream>
+
+using namespace std;
+
+void tablaMultiplo(int, int);
+
+int main(){
+    int m,n;
+
+    cout << "Cual tabla deseas? ";
+    cin >> n;
+
+    cout << "Cuantos multiplos deseas? ";
+    cin >> m;
+
+    tablaMultiplo(n,m);
+
+    system("pause");
+    return 0;
+}
+
+void tablaMultiplo(int n, int m){
+    cout << " Tabla de multiplicar del "<<n<< endl;
+
+    for(int i = 0; i <= m; i++){
+        cout << i << " x " << n << " = " <<i*n<<endl;
+    }
+}
